1fe8ad091a693d4a8525bd92b510c16efa26b4dc
Harfull <93506366+Harfull@users.noreply.github.com>
2025-12-02 22:03:37 -0500

    Migrate to Paper Plugin with plugin-yml Gradle Plugin
    Converted the project to a Paper plugin using the plugin-yml Gradle plugin.

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,5 +1,5 @@
-import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 import net.minecrell.pluginyml.paper.PaperPluginDescription
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 
 plugins {
     kotlin("jvm")
@@ -11,8 +11,15 @@ plugins {
 }
 
 group = "io.oira"
+version = "1.0"
+val targetJavaVersion = 21
+
+kotlin {
+    jvmToolchain(targetJavaVersion)
+}
 
 repositories {
+    mavenCentral()
     maven("https://repo.flyte.gg/releases")
     maven("https://repo.tcoded.com/releases")
     maven("https://maven.enginehub.org/repo/")
@@ -21,55 +28,76 @@ repositories {
 }
 
 dependencies {
+
     implementation(project(":api"))
 
     compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
     paperweight.paperDevBundle("1.21-R0.1-SNAPSHOT")
 
-    library("io.github.revxrsal:lamp.common:4.0.0-rc.13")
-    compileOnly("io.github.revxrsal:lamp.bukkit:4.0.0-rc.13")
-    compileOnly("io.github.revxrsal:lamp.brigadier:4.0.0-rc.13")
+    // Kotlin
+    paperLibrary("org.jetbrains.kotlin", "kotlin-stdlib-jdk8")
+    paperLibrary("org.jetbrains.kotlinx", "kotlinx-serialization-json", "1.6.0")
 
-    compileOnly("com.zaxxer:HikariCP:7.0.2")
-    compileOnly("org.jetbrains.exposed:exposed-core:0.61.0")
-    compileOnly("org.jetbrains.exposed:exposed-dao:0.61.0")
-    compileOnly("org.jetbrains.exposed:exposed-jdbc:0.61.0")
-    compileOnly("org.jetbrains.exposed:exposed-java-time:0.61.0")
-    compileOnly("org.jetbrains.exposed:exposed-kotlin-datetime:0.61.0")
+    // Lamp
+    paperLibrary("io.github.revxrsal", "lamp.common", "4.0.0-rc.13")
+    paperLibrary("io.github.revxrsal", "lamp.bukkit", "4.0.0-rc.13")
+    paperLibrary("io.github.revxrsal", "lamp.brigadier", "4.0.0-rc.13")
 
-    compileOnly("org.xerial:sqlite-jdbc:3.50.3.0")
-    compileOnly("com.mysql:mysql-connector-j:9.4.0")
-    compileOnly("com.h2database:h2:2.4.240")
+    // Database
+    paperLibrary("com.zaxxer", "HikariCP", "7.0.2")
+    paperLibrary("org.jetbrains.exposed", "exposed-core", "0.61.0")
+    paperLibrary("org.jetbrains.exposed", "exposed-dao", "0.61.0")
+    paperLibrary("org.jetbrains.exposed", "exposed-jdbc", "0.61.0")
+    paperLibrary("org.jetbrains.exposed", "exposed-java-time", "0.61.0")
+    paperLibrary("org.jetbrains.exposed", "exposed-kotlin-datetime", "0.61.0")
 
-    compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    paperLibrary("org.xerial", "sqlite-jdbc", "3.50.3.0")
+    paperLibrary("com.mysql", "mysql-connector-j", "9.4.0")
+    paperLibrary("com.h2database", "h2", "2.4.240")
 
+    // Others
+    paperLibrary("de.rapha149.signgui", "signgui", "2.5.4")
+    paperLibrary("com.tcoded", "FoliaLib", "0.5.1")
+    paperLibrary("redis.clients", "jedis", "5.1.0")
+
+    // Not embedded
     compileOnly("com.github.MilkBowl:VaultAPI:1.7")
     compileOnly("me.clip:placeholderapi:2.11.6")
     compileOnly("io.github.miniplaceholders:miniplaceholders-api:3.0.1")
     compileOnly("io.github.miniplaceholders:miniplaceholders-kotlin-ext:3.0.1")
-
-    implementation("de.rapha149.signgui:signgui:2.5.4")
-    implementation("com.tcoded:FoliaLib:0.5.1")
-
-    compileOnly("redis.clients:jedis:5.1.0")
 }
 
-val targetJavaVersion = 21
+paper {
+    name = "FluxEco"
+    version = project.version.toString()
+    main = "io.oira.fluxeco.FluxEco"
+    apiVersion = "1.21"
+    foliaSupported = true
 
-kotlin {
-    jvmToolchain(targetJavaVersion)
+    // Dependencies
+    serverDependencies {
+        register("Vault") {
+            required = true
+            load = PaperPluginDescription.RelativeLoadOrder.BEFORE
+        }
+        register("PlaceholderAPI") {
+            required = false
+        }
+        register("MiniPlaceholders") {
+            required = false
+        }
+    }
+
+    generateLibrariesJson = true
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    compilerOptions {
-        freeCompilerArgs.set(listOf("-Xannotation-default-target=param-property"))
-    }
+    compilerOptions.freeCompilerArgs.set(
+        listOf("-Xannotation-default-target=param-property")
+    )
 }
 
-tasks.jar {
-    enabled = false
-}
+tasks.jar { enabled = false }
 
 tasks.shadowJar {
     archiveBaseName.set("FluxEco")
@@ -83,7 +111,14 @@ tasks.shadowJar {
     relocate("com.tcoded.folialib", "io.oira.fluxeco.lib.folialib")
     relocate("de.rapha149.signgui", "io.oira.fluxeco.lib.signgui")
 
-    exclude("kotlin/**", "kotlinx/**", "org/jetbrains/**", "org/intellij/**", "META-INF/kotlin*", "META-INF/*.kotlin_module")
+    exclude(
+        "kotlin/**",
+        "kotlinx/**",
+        "org/jetbrains/**",
+        "org/intellij/**",
+        "META-INF/kotlin*",
+        "META-INF/*.kotlin_module"
+    )
 
     from(project(":api").sourceSets["main"].output)
 }
@@ -94,17 +129,4 @@ tasks.clean {
 
 tasks.build {
     dependsOn(tasks.shadowJar)
-}
-
-tasks.processResources {
-    val props = mapOf("version" to version)
-    inputs.properties(props)
-    filteringCharset = "UTF-8"
-    filesMatching("plugin.yml") {
-        expand(props)
-    }
-}
-
-tasks.runServer {
-    minecraftVersion("1.21")
 }
